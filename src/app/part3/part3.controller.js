6fa8502391389f2c97c2b9c2985f9604d3b699f8
Douglas Miller <douglas.miller@geant.org>
2020-02-08 11:03:09 +0000

    fixing video name

--- src/app/part3/part3.controller.js
+++ src/app/part3/part3.controller.js
@@ -302,7 +302,7 @@
      */
     vm.showVideo = function(video) {
       vm.video = video;
-      alert("video");
+      alert("video: " + video );
 
       var videocontainer = document.getElementById('video-1');
       var videosource = document.getElementById('videoSrc');
